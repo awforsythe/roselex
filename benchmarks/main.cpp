822fdc87f21d7571e08ab28a65376454b2a45fcd
Alex Forsythe <awforsythe@gmail.com>
2025-05-11 13:29:58 -0400

    Add benchmarks target

---
+++ benchmarks/main.cpp
@@ -0,0 +1,19 @@
+#include <cstdio>
+
+#include "rl_types.h"
+#include "rl_dawg.h"
+
+int main(int argc, char* argv[])
+{
+    if (argc < 2)
+    {
+        fprintf(stderr, "ERROR: Path to word list must be specified as first argument.\n");
+        return 1;
+    }
+    const char* wordlist_path = argv[1];
+
+    rl_dawg dawg;
+    const int32 num_words = rl_dawg_build(dawg, wordlist_path);
+    printf("Loaded %d word(s).\n", num_words);
+    return 0;
+}
