e74d957053d14a8a52a07c84e56dd3a6131c85fd
Alex Forsythe <awforsythe@gmail.com>
2025-05-15 11:24:39 -0400

    Make benchmark more configurable, automate gathering of CSV data

--- benchmarks/main.cpp
+++ benchmarks/main.cpp
@@ -25,35 +25,93 @@ struct TimeSample {
 	}
 };
 
-const int32 BOARD_SIZE_X = 500;
-const int32 BOARD_SIZE_Y = BOARD_SIZE_X;
+int32 seed = 0xfeeefeee;
 
-const int32 NUM_TILES_TO_DRAW = 50;
-const int32 NUM_MOVES_TO_PLAY = 500;
+int32 board_size_x = 500;
+int32 board_size_y = board_size_x;
+
+int32 first_move_x = 4;
+int32 first_move_y = 4;
+const char* first_move_word = "yesterday";
+
+int32 num_tiles_to_draw = 50;
+int32 num_moves_to_play = 500;
+int32 num_searches_to_play = 10;
+
+bool print_board = false;
 
 int main(int argc, char* argv[])
 {
-	TimeSample ts;
-	srand(0xfeeefeee);
-
 	if (argc < 2)
 	{
 		fprintf(stderr, "ERROR: Path to word list must be specified as first argument.\n");
 		return 1;
 	}
 	const char* wordlist_path = argv[1];
-	
-	bool csv_output = false;
-	if (argc > 2 && strcmp(argv[2], "--csv") == 0)
+
+	for (int32 i = 2; i < argc; i++)
 	{
-		csv_output = true;	
+		if (strstr(argv[i], "--seed="))
+		{
+			seed = atoi(argv[i]+7);
+		}
+		else if (strstr(argv[i], "--board-size-x="))
+		{
+			board_size_x = atoi(argv[i]+15);
+		}
+		else if (strstr(argv[i], "--board-size-y="))
+		{
+			board_size_y = atoi(argv[i]+15);
+		}
+		else if (strstr(argv[i], "--first-move-x="))
+		{
+			first_move_x = atoi(argv[i]+15);
+		}
+		else if (strstr(argv[i], "--first-move-y="))
+		{
+			first_move_y = atoi(argv[i]+15);
+		}
+		else if (strstr(argv[i], "--first-move-word="))
+		{
+			first_move_word = argv[i]+18;
+		}
+		else if (strstr(argv[i], "--num-tiles="))
+		{
+			num_tiles_to_draw = atoi(argv[i]+12);
+		}
+		else if (strstr(argv[i], "--num-moves="))
+		{
+			num_moves_to_play = atoi(argv[i]+12);
+		}
+		else if (strstr(argv[i], "--num-searches="))
+		{
+			num_searches_to_play = atoi(argv[i]+15);
+		}
+		else if (strstr(argv[i], "--print-board"))
+		{
+			print_board = true;
+		}
 	}
+	printf("seed: %d\n", seed);
+	printf("board-size-x: %d\n", board_size_x);
+	printf("board-size-y: %d\n", board_size_y);
+	printf("num-tiles: %d\n", num_tiles_to_draw);
+	printf("num-moves: %d\n", num_moves_to_play);
+	printf("num-searches: %d\n", num_searches_to_play);
+	printf("first-move-x: %d\n", first_move_x);
+	printf("first-move-y: %d\n", first_move_y);
+	printf("first-move-word: %s\n", first_move_word);
+
+	TimeSample ts;
+	srand(seed);
 
 	ts.start();
 	rl_dawg dawg;
 	memset(&dawg, 0, sizeof(dawg));
 	const int32 num_words = rl_dawg_build(dawg, wordlist_path);
 	const long long elapsed_load = ts.stop();
+	printf("num-words: %d\n", num_words);
+	printf("elapsed(load): %lld ns\n", elapsed_load);
 
 	// Draw the desired number of tiles, using the default letter distribution
 	rl_bag bag;
@@ -61,53 +119,60 @@ int main(int argc, char* argv[])
 
 	rl_rack rack;
 	rl_rack_init(rack);
-	for (int32 i = 0; i < NUM_TILES_TO_DRAW; i++)
+	for (int32 i = 0; i < num_tiles_to_draw; i++)
 	{
 		rl_rack_push(rack, rl_bag_draw(bag));
 	}
 
 	// Prepare a board with the desired dimensions
 	rl_board board;
-	rl_board_init(board, BOARD_SIZE_X, BOARD_SIZE_Y);
+	rl_board_init(board, board_size_x, board_size_y);
 
 	// Hardcode an initial move to populate the board
 	rl_move move;
 	rl_move_init(move);
-	move.index = rl_board_index(board, 4, 4);
+	move.index = rl_board_index(board, first_move_x, first_move_y);
 	move.offset = 1;
-	move.word[0] = 'y';
-	move.word[1] = 'e';
-	move.word[2] = 's';
-	move.word[3] = 't';
-	move.word[4] = 'e';
-	move.word[5] = 'r';
-	move.word[6] = 'd';
-	move.word[7] = 'a';
-	move.word[8] = 'y';
-	move.word_len = 9;
-	rl_rack_push(move.letters_used, 'y');
-	rl_rack_push(move.letters_used, 'e');
-	rl_rack_push(move.letters_used, 's');
-	rl_rack_push(move.letters_used, 't');
-	rl_rack_push(move.letters_used, 'e');
-	rl_rack_push(move.letters_used, 'r');
-	rl_rack_push(move.letters_used, 'd');
-	rl_rack_push(move.letters_used, 'a');
-	rl_rack_push(move.letters_used, 'y');
+	move.word_len = strlen(first_move_word);
+	for (int32 i = 0; i < move.word_len; i++)
+	{
+		move.word[i] = first_move_word[i];
+		rl_rack_push(move.letters_used, first_move_word[i]);
+	}
 
+	// Play the desired number of moves, zigzagging across the board and searching in
+	// arbitrary segments to chain the moves together
 	ts.start();
 	int32 num_moves_played = 0;
-	for (int32 i = 0; i < NUM_MOVES_TO_PLAY; i++)
+	while (true)
 	{
+		// Play the current move
 		rl_board_write(dawg, board, move.index, move.offset == 1, move.word, move.word_len);
 		rl_rack_subtract(rack, move.letters_used);
 		num_moves_played++;
+
+		// Replenish the rack to replace the letters played
 		for (int32 j = 0; j < move.word_len; j++)
 		{
 			rl_rack_push(rack, rl_bag_draw(bag));
 		}
 
-		const int32 num_moves_found = rl_search_board(dawg, board, rack, move);
+		if (num_moves_played == num_moves_to_play)
+		{
+			break;
+		}
+
+		// Find the next move to play, by searching perpendicular to the move we just
+		// played, near the end of the word
+		const bool across = num_moves_played % 2 == 0;
+		const int32 advance_from_start_of_prev_move = move.word_len - (rand() % 2) - 1;
+		const int32 index_of_intersect_with_prev_move = move.index + (advance_from_start_of_prev_move * move.offset);
+		const int32 prefix_len = (rand() % 2) + 1;
+		const int32 retreat_to_start_of_next_move = rl_board_offset(board, across) * prefix_len;
+		const int32 next_move_start_index = index_of_intersect_with_prev_move - retreat_to_start_of_next_move;
+		const int32 next_move_length = (rand() % 3) + 5;
+		const int32 num_moves_found = rl_search_segment(dawg, board, rack, next_move_start_index, nullptr, next_move_length, across, move);
+		printf("move(%d)found: %d\n", num_moves_played, num_moves_found);
 		if (num_moves_found == 0)
 		{
 			break;
@@ -115,17 +180,52 @@ int main(int argc, char* argv[])
 	}
 	const long long elapsed_moves = ts.stop();
 
-	if (csv_output)
+	printf("num-moves-played: %d\n", num_moves_played);
+	printf("elapsed(moves): %lld ns\n", elapsed_moves);
+
+	//
+	ts.start();
+	int32 num_searches_played = 0;
+	for (int32 i = 0; i < num_searches_to_play; i++)
 	{
-		printf("%d,%d,%d,%lld,%lld\n", num_words, NUM_TILES_TO_DRAW, NUM_MOVES_TO_PLAY, elapsed_load, elapsed_moves);
+		const int32 num_moves_found = rl_search_board(dawg, board, rack, move);
+		printf("search(%d)found: %d\n", i, num_moves_found);
+		if (num_moves_found == 0)
+		{
+			break;
+		}
+
+		rl_board_write(dawg, board, move.index, move.offset == 1, move.word, move.word_len);
+		rl_rack_subtract(rack, move.letters_used);
+		num_searches_played++;
+		for (int32 j = 0; j < move.word_len; j++)
+		{
+			rl_rack_push(rack, rl_bag_draw(bag));
+		}
 	}
-	else
+	const long long elapsed_searches = ts.stop();
+
+	printf("num-searches-played: %d\n", num_searches_played);
+	printf("elapsed(searches): %lld ns\n", elapsed_searches);
+
+	if (print_board)
 	{
-		printf("words: %d\n", num_words);
-		printf("tiles: %d\n", NUM_TILES_TO_DRAW);
-		printf("moves: %d\n", NUM_MOVES_TO_PLAY);
-		printf("elapsed(load): %lld ns\n", elapsed_load);
-		printf("elapsed(moves): %lld ns\n", elapsed_moves);
+		for (int32 y = 0; y < board_size_y; y++)
+		{
+			for (int32 x = 0; x < board_size_x; x++)
+			{
+				const int32 index = rl_board_index(board, x, y);
+				if (board.letters[index] == RL_BLANK || board.letters[index] == RL_ANCHOR)
+				{
+					putchar('.');
+				}
+				else
+				{
+					putchar(board.letters[index]);
+				}
+			}
+			putchar('\n');
+		}
 	}
 
 	rl_board_free(board);
--- run-benchmarks.sh
+++ run-benchmarks.sh
@@ -1,66 +1,162 @@
 #!/usr/bin/env bash
 set -e
 
+# Build a profile-optimized version of our benchmark executable
 BUILD_DIR='./build_bench'
 if [ ! -d "$BUILD_DIR" ]; then
   mkdir "$BUILD_DIR"
   cmake -B"$BUILD_DIR" -DCMAKE_BUILD_TYPE=RelWithDebInfo .
 fi
-cmake --build "$BUILD_DIR"
+cmake --build "$BUILD_DIR" >/dev/null 2>&1
 
-echo ""
-echo "wordlist,num_allocs,num_frees,bytes_allocated,load_min,load_max,load_median,load_mean,load_stddev,moves_min,moves_max,moves_median,moves_mean,moves_stddev"
-WORDLISTS='basic'
-NUM_EXECUTIONS_PER_WORDLIST='10'
-for WORDLIST in $WORDLISTS; do
-  # Run valgrind (memcheck), failing if any leaks/errors are detected, and capture the
-  # memory usage stats from our benchmark program
-  echo -n "$WORDLIST"
+# Run valgrind/memcheck to profile allocation stats for building word lists
+profile_allocation() {
+  WORDLIST="$1"
   ALLOC_STATS=$(
     valgrind \
       --tool=memcheck \
       --leak-check=full \
       --error-exitcode=1 \
       --exit-on-first-error=yes \
-      "$BUILD_DIR/benchmarks" "./data/words_$WORDLIST.txt" 2>&1 | \
+      "$BUILD_DIR/benchmarks" "$WORDLIST" \
+        --seed=12345 \
+        --board-size-x=200 \
+        --board-size-y=180 \
+        --num-tiles=50 \
+        --num-moves=1 \
+        --num-searches=0 \
+        --first-move-x=4 \
+        --first-move-y=4 \
+        --first-move-word=yesterday \
+      2>&1 | \
         grep 'total heap usage' | \
         sed -E 's/.*: ([0-9,]+) allocs, ([0-9,]+) frees, ([0-9,]+) bytes allocated/\1 \2 \3/' | \
         tr -d ','
   )
   NUM_ALLOCATIONS=$(echo "$ALLOC_STATS" | cut -d' ' -f1)
-  NUM_FREES=$(echo "$ALLOC_STATS" | cut -d' ' -f2)
   BYTES_ALLOCATED=$(echo "$ALLOC_STATS" | cut -d' ' -f3)
-  echo -n ",$NUM_ALLOCATIONS,$NUM_FREES,$BYTES_ALLOCATED"
+  echo "$NUM_ALLOCATIONS,$BYTES_ALLOCATED"
+}
 
-  # Run our benchmark several times, capturing the elapsed time taken to load the word
-  # list and to search for and make several moves on the board
-  ELAPSED_NS_LOAD_PY_LITERALS=""
-  ELAPSED_NS_MOVES_PY_LITERALS=""
-  for I in $(seq $NUM_EXECUTIONS_PER_WORDLIST); do
+profile_peak_heap_usage() {
+  rm -f ./massif.out
+  WORDLIST="$1"
+  valgrind \
+    --tool=massif \
+    --massif-out-file=./massif.out \
+    "$BUILD_DIR/benchmarks" "$WORDLIST" \
+      --seed=12345 \
+      --board-size-x=200 \
+      --board-size-y=180 \
+      --num-tiles=50 \
+      --num-moves=1 \
+      --num-searches=0 \
+      --first-move-x=4 \
+      --first-move-y=4 \
+      --first-move-word=yesterday \
+  >/dev/null 2>&1
+  PEAK_HEAP_BYTES=$(cat ./massif.out | grep mem_heap_B= | cut -d'=' -f2 | sort -n | tail -1)
+  rm -f massif.out
+  echo "$PEAK_HEAP_BYTES"
+}
+
+# Profile the time it takes to perform word list building and move solving
+analyze_execution_stats() {
+  PY_LITERALS="$1"
+  MIN=$(python3 -c "print(min([$PY_LITERALS]))")
+  MAX=$(python3 -c "print(max([$PY_LITERALS]))")
+  MEDIAN=$(python3 -c "import statistics; print(int(statistics.median([$PY_LITERALS])))")
+  MEAN=$(python3 -c "import statistics; print(int(statistics.mean([$PY_LITERALS])))")
+  STDEV=$(python3 -c "import statistics; print(int(statistics.stdev([$PY_LITERALS])))")
+  echo "$MIN,$MAX,$MEDIAN,$MEAN,$STDEV"
+}
+
+profile_execution_alpha() {
+  LOAD_LITERALS=""
+  MOVES_LITERALS=""
+  for I in {1..10}; do
+    BENCHMARK_OUTPUT=$(
+      "$BUILD_DIR/benchmarks" ./data/words_alpha.txt \
+        --seed=12345 \
+        --board-size-x=200 \
+        --board-size-y=180 \
+        --num-tiles=50 \
+        --num-moves=100 \
+        --num-searches=0 \
+        --first-move-x=4 \
+        --first-move-y=4 \
+        --first-move-word=yesterday
+    )
     if [ "$I" -gt 1 ]; then
-      ELAPSED_NS_LOAD_PY_LITERALS="$ELAPSED_NS_LOAD_PY_LITERALS,"
-      ELAPSED_NS_MOVES_PY_LITERALS="$ELAPSED_NS_MOVES_PY_LITERALS,"
+      LOAD_LITERALS="$LOAD_LITERALS,"
+      MOVES_LITERALS="$MOVES_LITERALS,"
     fi
-    BENCHMARK_OUTPUT=$("$BUILD_DIR/benchmarks" "./data/words_$WORDLIST.txt")
     ELAPSED_NS_LOAD=$(echo "$BENCHMARK_OUTPUT" | grep -F 'elapsed(load): ' | cut -d' ' -f2)
     ELAPSED_NS_MOVES=$(echo "$BENCHMARK_OUTPUT" | grep -F 'elapsed(moves): ' | cut -d' ' -f2)
-    ELAPSED_NS_LOAD_PY_LITERALS="$ELAPSED_NS_LOAD_PY_LITERALS$ELAPSED_NS_LOAD"
-    ELAPSED_NS_MOVES_PY_LITERALS="$ELAPSED_NS_MOVES_PY_LITERALS$ELAPSED_NS_MOVES"
+    LOAD_LITERALS="$LOAD_LITERALS$ELAPSED_NS_LOAD"
+    MOVES_LITERALS="$MOVES_LITERALS$ELAPSED_NS_MOVES"
   done
+  LOAD_STATS=$(analyze_execution_stats "$LOAD_LITERALS")
+  MOVES_STATS=$(analyze_execution_stats "$MOVES_LITERALS")
+  echo "$LOAD_STATS,$MOVES_STATS"
+}
 
-  LOAD_MIN=$(python3 -c "print(min([$ELAPSED_NS_LOAD_PY_LITERALS]))")
-  LOAD_MAX=$(python3 -c "print(max([$ELAPSED_NS_LOAD_PY_LITERALS]))")
-  LOAD_MEDIAN=$(python3 -c "import statistics; print(int(statistics.median([$ELAPSED_NS_LOAD_PY_LITERALS])))")
-  LOAD_MEAN=$(python3 -c "import statistics; print(int(statistics.mean([$ELAPSED_NS_LOAD_PY_LITERALS])))")
-  LOAD_STDEV=$(python3 -c "import statistics; print(int(statistics.stdev([$ELAPSED_NS_LOAD_PY_LITERALS])))")
-  echo -n "$LOAD_MIN,$LOAD_MAX,$LOAD_MEDIAN,$LOAD_MEAN,$LOAD_STDEV"
+profile_execution_basic() {
+  LOAD_LITERALS=""
+  SEARCHES_LITERALS=""
+  for I in {1..10}; do
+    BENCHMARK_OUTPUT=$(
+      "$BUILD_DIR/benchmarks" ./data/words_basic.txt \
+        --seed=12345 \
+        --board-size-x=200 \
+        --board-size-y=180 \
+        --num-tiles=50 \
+        --num-moves=0 \
+        --num-searches=200 \
+        --first-move-x=4 \
+        --first-move-y=4 \
+        --first-move-word=yesterday
+    )
+    if [ "$I" -gt 1 ]; then
+      LOAD_LITERALS="$LOAD_LITERALS,"
+      SEARCHES_LITERALS="$SEARCHES_LITERALS,"
+    fi
+    ELAPSED_NS_LOAD=$(echo "$BENCHMARK_OUTPUT" | grep -F 'elapsed(load): ' | cut -d' ' -f2)
+    ELAPSED_NS_SEARCHES=$(echo "$BENCHMARK_OUTPUT" | grep -F 'elapsed(searches): ' | cut -d' ' -f2)
+    LOAD_LITERALS="$LOAD_LITERALS$ELAPSED_NS_LOAD"
+    SEARCHES_LITERALS="$SEARCHES_LITERALS$ELAPSED_NS_SEARCHES"
+  done
+  LOAD_STATS=$(analyze_execution_stats "$LOAD_LITERALS")
+  SEARCHES_STATS=$(analyze_execution_stats "$SEARCHES_LITERALS")
+  echo "$LOAD_STATS,$SEARCHES_STATS"
+}
 
-  MOVES_MIN=$(python3 -c "print(min([$ELAPSED_NS_MOVES_PY_LITERALS]))")
-  MOVES_MAX=$(python3 -c "print(max([$ELAPSED_NS_MOVES_PY_LITERALS]))")
-  MOVES_MEDIAN=$(python3 -c "import statistics; print(int(statistics.median([$ELAPSED_NS_MOVES_PY_LITERALS])))")
-  MOVES_MEAN=$(python3 -c "import statistics; print(int(statistics.mean([$ELAPSED_NS_MOVES_PY_LITERALS])))")
-  MOVES_STDEV=$(python3 -c "import statistics; print(int(statistics.stdev([$ELAPSED_NS_MOVES_PY_LITERALS])))")
-  echo -n ",$MOVES_MIN,$MOVES_MAX,$MOVES_MEDIAN,$MOVES_MEAN,$MOVES_STDEV"
+echo -n 'basic_num_allocations,basic_bytes_allocated'
+echo -n ',basic_peak_heap'
+echo -n ',basic_load_min,basic_load_max,basic_load_median,basic_load_mean,basic_load_stdev'
+echo -n ',basic_searches_min,basic_searches_max,basic_searches_median,basic_searches_mean,basic_searches_stdev'
+echo -n ',alpha_num_allocations,alpha_bytes_allocated'
+echo -n ',alpha_peak_heap'
+echo -n ',alpha_load_min,alpha_load_max,alpha_load_median,alpha_load_mean,alpha_load_stdev'
+echo -n ',alpha_moves_min,alpha_moves_max,alpha_moves_median,alpha_moves_mean,alpha_moves_stdev'
+echo ''
 
-  echo ""
-done
+BASIC_ALLOC_STATS=$(profile_allocation ./data/words_basic.txt)
+echo -n "$BASIC_ALLOC_STATS"
+
+BASIC_PEAK_HEAP=$(profile_peak_heap_usage ./data/words_basic.txt)
+echo -n ",$BASIC_PEAK_HEAP"
+
+EXECUTION_BASIC_STATS=$(profile_execution_basic)
+echo -n ",$EXECUTION_BASIC_STATS"
+
+ALPHA_ALLOC_STATS=$(profile_allocation ./data/words_alpha.txt)
+echo -n ",$ALPHA_ALLOC_STATS"
+
+ALPHA_PEAK_HEAP=$(profile_peak_heap_usage ./data/words_alpha.txt)
+echo -n ",$ALPHA_PEAK_HEAP"
+
+EXECUTION_ALPHA_STATS=$(profile_execution_alpha)
+echo -n ",$EXECUTION_ALPHA_STATS"
+
+echo ''
